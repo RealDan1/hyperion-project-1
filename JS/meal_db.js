8b970fefb6c24adb79b8e4bd6a048b65494b5d98
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-08-24 12:54:04 +0200

    feat: add randomiser funcitonality to the array choice

--- JS/meal_db.js
+++ JS/meal_db.js
@@ -4,5 +4,11 @@ fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=garlic_powder')
   //parse the network response from JSON
   .then((res) => res.json())
   //then use the created object
-  .then((listOfMeals) => console.log(listOfMeals.meals[0]));
+
+  .then((listOfMeals) =>
+    console.log(
+      listOfMeals.meals[Math.floor(Math.random() * listOfMeals.meals.length)]
+        .strMeal
+    )
+  );
 //note we have to write a function that randomises the chosen meal
