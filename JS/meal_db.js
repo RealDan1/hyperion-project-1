b684b80633841d45b43f9e418bd58ca4e23fbdec
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-08-24 12:58:57 +0200

    refact: comments

--- JS/meal_db.js
+++ JS/meal_db.js
@@ -3,7 +3,6 @@ let meal;
 fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=garlic_powder')
   //parse the network response from JSON
   .then((res) => res.json())
-  //then use the created object
 
   .then((listOfMeals) => {
     //choose a random array item number(done on a separate line for readability)
@@ -11,4 +10,3 @@ fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=garlic_powder')
     //log the .strMeal (name) of the meal
     console.log(listOfMeals.meals[randomItemNumber].strMeal);
   });
-//note we have to write a function that randomises the chosen meal
