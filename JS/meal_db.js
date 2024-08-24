efa95fa144558970b43cbd884093ae69fcfd9c09
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-08-24 12:25:03 +0200

    feat: finish the core API call functionality

--- JS/meal_db.js
+++ JS/meal_db.js
@@ -1,1 +1,8 @@
-let meal = fetch();
+let meal;
+//send get request to server for a meal
+fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=garlic_powder')
+  //parse the network response from JSON
+  .then((res) => res.json())
+  //then use the created object
+  .then((listOfMeals) => console.log(listOfMeals.meals[0]));
+//note we have to write a function that randomises the chosen meal
