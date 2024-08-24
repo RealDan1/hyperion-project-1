01c435f96f9d888d3d574d97c912b600c3df976a
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-08-24 12:58:11 +0200

    refact: neaten the code for readability

--- JS/meal_db.js
+++ JS/meal_db.js
@@ -5,10 +5,10 @@ fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=garlic_powder')
   .then((res) => res.json())
   //then use the created object
 
-  .then((listOfMeals) =>
-    console.log(
-      listOfMeals.meals[Math.floor(Math.random() * listOfMeals.meals.length)]
-        .strMeal
-    )
-  );
+  .then((listOfMeals) => {
+    //choose a random array item number(done on a separate line for readability)
+    let randomItemNumber = Math.floor(Math.random() * listOfMeals.meals.length);
+    //log the .strMeal (name) of the meal
+    console.log(listOfMeals.meals[randomItemNumber].strMeal);
+  });
 //note we have to write a function that randomises the chosen meal
