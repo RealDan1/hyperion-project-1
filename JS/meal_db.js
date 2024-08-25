dacad6bcee4736403180e24a6df3c81b72f83e37
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-08-25 18:08:46 +0200

    feat: start sessionstorage part

--- JS/meal_db.js
+++ JS/meal_db.js
@@ -22,14 +22,18 @@ fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=avocado')
     //set this item as the order and then initiate the order
 
     //create a new Meal instance with the selected item and print it.
-    let order1 = new Meal(
-      listOfMeals.meals[randomItemNumber].strMeal,
-      1,
-      false
-    );
-    order1.print();
+    let order = new Meal(listOfMeals.meals[randomItemNumber].strMeal, 1, false);
+    order.print();
 
-    // sessionStorage.setItem("");
+    sessionStorage.setItem(
+      `orderNumber${order.orderNumber}`,
+      JSON.stringify(order)
+    );
+
+    let returnedMeal = JSON.parse(
+      sessionStorage.getItem(`meal${order.orderNumber}`)
+    );
+    console.log(`this is the returned ${returnedMeal.description}`);
 
     //WRITE A CASE FOR IF THE RETURNED OBJECT IS NULL INCASE WE TYPE IN THE WRONG NAME
   });
