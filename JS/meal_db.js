bcf85949a068c3d6fc901a275ba5ee27ced66aae
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-08-25 17:50:39 +0200

    feat: switch object constructor to class

--- JS/meal_db.js
+++ JS/meal_db.js
@@ -1,4 +1,15 @@
-let meal;
+//create the meal class - my task reviewer said I should start getting used to using classes and experiment with them:
+class Meal {
+  constructor(description, orderNumber, completionStatus) {
+    this.description = description;
+    this.orderNumber = orderNumber;
+    this.completionStatus = completionStatus;
+  }
+
+  print() {
+    console.log(`this meal is ${this.description}`);
+  }
+}
 //send get request to server for a meal
 fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=garlic_powder')
   //parse the network response from JSON
@@ -8,14 +19,15 @@ fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=garlic_powder')
     //choose a random array item number(done on a separate line for readability)
     let randomItemNumber = Math.floor(Math.random() * listOfMeals.meals.length);
     //log the .strMeal (name) of the meal
-    console.log(listOfMeals.meals[randomItemNumber].strMeal);
+    // console.log(listOfMeals.meals[randomItemNumber].strMeal);
     //set this item as the order and then initiate the order
 
-    //create the meal object constructor:
-    function meal(description, orderNumber, completionStatus) {
-      this.description = description;
-      this.orderNumber = orderNumber;
-      this.completionStatus = completionStatus;
-    }
+    let order1 = new Meal(
+      listOfMeals.meals[randomItemNumber].strMeal,
+      1,
+      false
+    );
+    order1.print();
+
     // sessionStorage.setItem("");
   });
