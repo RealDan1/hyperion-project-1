3af8079d08c968b08468bbe453d3a142fa852fc6
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-08-25 16:42:13 +0200

    feat: start object constructor - needs to be class

--- JS/meal_db.js
+++ JS/meal_db.js
@@ -10,4 +10,12 @@ fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=garlic_powder')
     //log the .strMeal (name) of the meal
     console.log(listOfMeals.meals[randomItemNumber].strMeal);
     //set this item as the order and then initiate the order
+
+    //create the meal object constructor:
+    function meal(description, orderNumber, completionStatus) {
+      this.description = description;
+      this.orderNumber = orderNumber;
+      this.completionStatus = completionStatus;
+    }
+    // sessionStorage.setItem("");
   });
