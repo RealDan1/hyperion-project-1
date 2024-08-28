3e0d39ca56cad488b015c595188a550f56df1aa7
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-08-28 13:06:12 +0200

    feat: attempt fix on the looping session storage bug (WIP)

--- JS/meal_db.js
+++ JS/meal_db.js
@@ -1,5 +1,5 @@
-//create the meal class - my task reviewer said I should start getting used to using classes and experiment with them:
-class Meal {
+//create the Order class - my task reviewer said I should start getting used to using classes and experiment with them:
+class Order {
   constructor(description, orderNumber, completionStatus) {
     this.description = description;
     this.orderNumber = orderNumber;
@@ -21,21 +21,34 @@ fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=avocado')
     let randomItemNumber = Math.floor(Math.random() * listOfMeals.meals.length);
 
     //set this item as the order and then initiate the order
+    //=====================================================
 
-    //create a new Meal instance with the selected item and print it.
-    let order = new Meal(listOfMeals.meals[randomItemNumber].strMeal, 1, false);
-    order.print();
+    //create an allOrders array to store all orders - the length of this array will also be used to determine how many times the page has been refreshed which will be useful later.
+    let allOrders = [];
+
+    //create a new Order instance with the selected item and push it to the array.
+    allOrders.push(
+      new Order(listOfMeals.meals[randomItemNumber].strMeal, 1, false)
+    );
+    console.log(allOrders);
 
     //SESSION STORAGE SECTION BUG: ITS NOW REFERENCING THE SAME MEAL BUT GENERATING NEW ONES UP TOP
-    if (sessionStorage.getItem('orderNumber${order.orderNumber}')) {
+    //if the ordernumber exists in sessionstorage??(dno if this is correct since there is no way to reference order numbers yet) then we just need to retrieve it from storage.
+
+    //refreshing the page will generate a new order number
+    if (sessionStorage.getItem(`orderNumber${order.orderNumber}`)) {
       // DELETE fill this - soemthing like get it again?
-      let retrievedOrder = sessionStorage.getItem(
-        'orderNumber${order.orderNumber}'
+      let retrievedOrder = sessionStorage.getItem(`allOrders`);
+      console.log(
+        'this is the retrieved orders now as an array of objects:\n' +
+          retrievedOrder
       );
     } else {
-      sessionStorage.setItem(
-        `orderNumber${order.orderNumber}`,
-        JSON.stringify(order)
+      //otherwise the order does not exist yet inside session storage so we need to save it into storage
+      sessionStorage.setItem(`allOrders`, JSON.stringify(allOrders));
+      console.log(
+        'this is still the old array, nothin exists yet only one object should be in here:\n' +
+          allOrders
       );
     }
 
