41193046f8bc7c5da5b3d77dfa384494587b9095
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-08-30 12:52:56 +0200

    feat: add skeleton of correct sessionstorage check

--- JS/meal_db.js
+++ JS/meal_db.js
@@ -11,6 +11,8 @@ class Order {
     console.log(`this meal is ${this.description}`);
   }
 }
+
+let allorders;
 //send get request to server for a meal
 fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=beef')
   //parse the network response from JSON
@@ -24,39 +26,53 @@ fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=beef')
     //=====================================================
 
     //create an allOrders array to store all orders - the length of this array will also be used to determine how many times the page has been refreshed which will be useful later.
-    let allOrders = [];
+    let allOrders;
 
     //create a new Order instance with the selected item and push it to the array.
-    allOrders.push(
-      new Order(listOfMeals.meals[randomItemNumber].strMeal, 1, false)
-    );
-    console.log('this is just a test to show the array initialised:');
-    console.log(allOrders);
 
-    sessionStorage.setItem(`allOrders`, JSON.stringify(allOrders));
-    console.log('retrieved:');
-    console.log(JSON.parse(sessionStorage.getItem(`allOrders`)));
+    if (sessionStorage.getItem(`allOrders`)) {
+      let retrievedOrders = JSON.parse(sessionStorage.getItem(`allOrders`));
+      retrievedOrders.push(
+        new Order(listOfMeals.meals[randomItemNumber].strMeal, 1, false)
+      );
+      console.log(
+        'i just added an order to the previously existing array - this should accumulate something now'
+      );
+      console.log(retrievedOrders);
+      sessionStorage.setItem(`allOrders`, JSON.stringify(retrievedOrders));
+    } else {
+      allorders.push(
+        new Order(listOfMeals.meals[randomItemNumber].strMeal, 1, false)
+      );
+      sessionStorage.setItem(`allOrders`, JSON.stringify(allOrders));
+    }
 
-    //SESSION STORAGE SECTION BUG: ITS NOW REFERENCING THE SAME MEAL BUT GENERATING NEW ONES UP TOP
-    //if the ordernumber exists in sessionstorage??(dno if this is correct since there is no way to reference order numbers yet) then we just need to retrieve it from storage.
+    // console.log('this is just a test to show the array initialised:');
+    // console.log(allOrders);
 
-    //refreshing the page will generate a new order number
-    //RETRIEVE allOrders before setting might cause a bug - but it will always set first
-    // if i understand this correctly - its gna push once to sessionSt and then never set again.
-    //then dont do an if?
-    // if (sessionStorage.getItem(`allOrders`)) {
-    //   // DELETE fill this - soemthing like get it again?
-    //   let retrievedOrder = sessionStorage.getItem(`allOrders`);
-    //   console.log('this is the retrieved orders, now as an array of objects:');
-    //   console.log(retrievedOrder);
-    // } else {
-    //   //otherwise the order does not exist yet inside session storage so we need to save it into storage
-    //   sessionStorage.setItem(`allOrders`, JSON.stringify(allOrders));
-    //   console.log(
-    //     'this is still the old array, nothin exists yet only one object should be in here:\n' +
-    //       allOrders
-    //   );
+    // sessionStorage.setItem(`allOrders`, JSON.stringify(allOrders));
+    // console.log('retrieved:');
+    // console.log(JSON.parse(sessionStorage.getItem(`allOrders`)));
   });
+//SESSION STORAGE SECTION BUG: ITS NOW REFERENCING THE SAME MEAL BUT GENERATING NEW ONES UP TOP
+//if the ordernumber exists in sessionstorage??(dno if this is correct since there is no way to reference order numbers yet) then we just need to retrieve it from storage.
+
+//refreshing the page will generate a new order number
+//RETRIEVE allOrders before setting might cause a bug - but it will always set first
+// if i understand this correctly - its gna push once to sessionSt and then never set again.
+//then dont do an if?
+// if (sessionStorage.getItem(`allOrders`)) {
+//   // DELETE fill this - soemthing like get it again?
+//   let retrievedOrder = sessionStorage.getItem(`allOrders`);
+//   console.log('this is the retrieved orders, now as an array of objects:');
+//   console.log(retrievedOrder);
+// } else {
+//   //otherwise the order does not exist yet inside session storage so we need to save it into storage
+//   sessionStorage.setItem(`allOrders`, JSON.stringify(allOrders));
+//   console.log(
+//     'this is still the old array, nothin exists yet only one object should be in here:\n' +
+//       allOrders
+//   );
 
 //do if statement to check for stored item, THEN follow up
 
