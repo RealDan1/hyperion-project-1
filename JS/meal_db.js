c0bdb412019aec80082773524a64268ef9d612db
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-09-01 17:43:32 +0200

    refact: optimise "currentOrder set feature" for readability

--- JS/meal_db.js
+++ JS/meal_db.js
@@ -11,10 +11,10 @@ class Order {
 
 // DELETE: PLACE ALL THIS INTO A FUNCTION, arg(mainIngredient) - returns meal for this round OR finishes the code up to adding to session storage. Perhaps we should make this a seperate function - this should run the core process of creating a new order and adding it. from there UI should dictate how we move the program around
 
-// Create a function to send a get request to the server for a meal using user input(prompt) as the mainIngredient
+// Create a function to send a get request to the API for a meal using prompt() as the mainIngredient
 // =====================================================
 function getMeal() {
-  //DELETE - still need to make this mainIngredient only take LOWERCASE AND WITH UNDERSCORE IN SPACES
+  // DELETE - still need to make this mainIngredient only take LOWERCASE AND WITH UNDERSCORE IN SPACES
 
   // DELETE ALSO - write a case for if the returned API object is "null" in which case a seemingly valid name was entered but they dont have it in the DBase
 
@@ -62,9 +62,11 @@ function setOrder(meal) {
     let ssAllOrders = JSON.parse(sessionStorage.getItem(`allOrders`));
     // Add the meal to the retrieved array
     ssAllOrders.push(meal);
-    // Create a unique order number and add it to the orderNumber value within the object. The number will be generated from the length of the ssAllOrders array +1
+    // Create a unique order number: The number will be generated from the length of the ssAllOrders array +1
     // POTENTIAL BUG - order number in array could be wrong it may be one too high
-    ssAllOrders[ssAllOrders.length + 1].orderNumber = ssAllOrders.length + 1;
+    let currentOrder = ssAllOrders.length + 1;
+    // and then add it to the orderNumber value within the object.
+    ssAllOrders[ssAllOrders.length + 1].orderNumber = currentOrder;
     // Console.log the order for testing
     console.log('Overwriting the array now, the new array is:');
     console.log(ssAllOrders);
@@ -72,8 +74,9 @@ function setOrder(meal) {
     sessionStorage.setItem(`allOrders`, JSON.stringify(ssAllOrders));
     //set the last ordernumber as the latest order in session storage (overwrite any previous value)
     //POTENTIAL BUG: orderNumber in sessionStorage could be the wrong number
-    let currentOrder = ssAllOrders.length + 1;
+
     sessionStorage.setItem(`currentOrder`, JSON.stringify(currentOrder));
+    //console.log currentOrderBumber
   } else {
     // Else add the meal to the first place in the array:
     allOrders.push(meal);
@@ -87,8 +90,13 @@ function setOrder(meal) {
 }
 
 // Initiate the function chain
-
-//NOTE: getMeal() contains promises so has to be followed by .then
+// =====================================================
+// Call getMeal() to ask the user for an ingredient and return a chosen dish from the API
 getMeal()
-  //
-  .then((meal) => setOrder(mealForThisRound));
+  //NOTE: getMeal() contains promises so has to be followed by .then
+  // Then use the returned meal and drop it into the setOrder function as an arg, setOrder will save the order to the array and then overwrite sessionStorage with the array (as well as set the current order number).
+  .then((meal) => setOrder(meal));
+
+// Then display the current meals - next part of activity
+//.then();
+//DELETE - PERHAPS THINK OF RECURSION HERE - after this last function is called, place the entire main function chain INSIDE another function, then call that function after the chain has been run. endless user loop
