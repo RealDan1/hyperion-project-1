ad7ff4a1df5c6a618c4a2e2793c128a1ae04c660
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-09-01 16:52:42 +0200

    feat: further partition setOrder() into function

--- JS/meal_db.js
+++ JS/meal_db.js
@@ -1,4 +1,4 @@
-//create the Order class - my task reviewer said I should start getting used to using classes and experiment with them:
+// Define Order class with properties for description, order number, and completion status - my task reviewer said I should start getting used to using classes and experiment with them:
 class Order {
   constructor(description, orderNumber, completionStatus) {
     this.description = description;
@@ -6,26 +6,32 @@ class Order {
     this.completionStatus = completionStatus;
   }
 }
-//DELETE: PLACE ALL THIS INTO A FUNCTION, arg(mainIngredient) - returns meal for this round OR finishes the code up to adding to session storage. Perhaps we should make this a seperate function - this should run the core process of creating a new order and adding it. from there UI should dictate how we move the program around
 
-//send get request to server for a meal
-function getMeal(mainIngredient) {
+// DELETE: PLACE ALL THIS INTO A FUNCTION, arg(mainIngredient) - returns meal for this round OR finishes the code up to adding to session storage. Perhaps we should make this a seperate function - this should run the core process of creating a new order and adding it. from there UI should dictate how we move the program around
+
+// create a function to send a get request to the server for a meal using user input(prompt) as the mainIngredient
+function getMeal() {
   //DELETE - still need to make this mainIngredient only take LOWERCASE AND WITH UNDERSCORE IN SPACES
-  //DELETE ALSO - write a case for if the returned API object is "null" in which case a seemingly valid name was entered but they dont have it in the DBase
+
+  // DELETE ALSO - write a case for if the returned API object is "null" in which case a seemingly valid name was entered but they dont have it in the DBase
+
+  let mainIngredient = prompt(
+    'please enter your (single) main ingredient of choice seperated by only spaces if its multiple words:'
+  );
   fetch(
     'https://www.themealdb.com/api/json/v1/1/filter.php?i=' + mainIngredient
   )
-    //parse the network response from JSON
+    // Parse the network response from JSON
     .then((res) => res.json())
 
-    //run main function code
+    // Run main function code
     .then((listOfMeals) => {
-      //choose a random array item number(i.e. choose a random meal). Done on a separate line for readability.
+      // Choose a random array item number(i.e. choose a random meal). Done on a separate line for readability.
       let randomItemNumber = Math.floor(
         Math.random() * listOfMeals.meals.length
       );
 
-      //create the newly generated meal so we can use it later
+      // Create the newly generated meal so we can use it later
       let meal = new Order(
         listOfMeals.meals[randomItemNumber].strMeal,
         1,
@@ -39,33 +45,37 @@ function getMeal(mainIngredient) {
     });
 }
 
-//initiate the function chain - NOTE: getMeal() contains promises so has to be followed by .then
-getMeal('beef').then();
+function setOrder(meal) {
+  //function to set an item as the order and then initiate the order
+  //=====================================================
 
-//function to set an item as the order and then initiate the order
-//=====================================================
+  //create an allOrders array to store all orders - the length of this array will also be used to determine how many times the page has been refreshed which will be useful later.
+  let allOrders = [];
 
-//create an allOrders array to store all orders - the length of this array will also be used to determine how many times the page has been refreshed which will be useful later.
-let allOrders = [];
+  //create a new Order instance with the selected item and push it to the array.
 
-//create a new Order instance with the selected item and push it to the array.
-
-if (sessionStorage.getItem(`allOrders`)) {
-  let ssAllOrders = JSON.parse(sessionStorage.getItem(`allOrders`));
-  ssAllOrders.push(mealForThisRound);
-  ssAllOrders[ssAllOrders.length + 1].orderNumber = ssAllOrders.length + 1;
-  console.log(
-    'i just added an order to the previously existing array - this should accumulate something now'
-  );
-  console.log(ssAllOrders);
-  sessionStorage.setItem(`allOrders`, JSON.stringify(ssAllOrders));
-} else {
-  allOrders.push(mealForThisRound);
-  sessionStorage.setItem(`allOrders`, JSON.stringify(allOrders));
-  console.log(
-    'i just added the new order to allOrders array and then stored it for the first time in session storage. the array contains:'
-  );
-  console.log(allOrders);
+  if (sessionStorage.getItem(`allOrders`)) {
+    let ssAllOrders = JSON.parse(sessionStorage.getItem(`allOrders`));
+    ssAllOrders.push(mealForThisRound);
+    ssAllOrders[ssAllOrders.length + 1].orderNumber = ssAllOrders.length + 1;
+    console.log(
+      'i just added an order to the previously existing array - this should accumulate something now'
+    );
+    console.log(ssAllOrders);
+    sessionStorage.setItem(`allOrders`, JSON.stringify(ssAllOrders));
+  } else {
+    allOrders.push(meal);
+    sessionStorage.setItem(`allOrders`, JSON.stringify(allOrders));
+    console.log(
+      'i just added the new order to allOrders array and then stored it for the first time in session storage. the array contains:'
+    );
+    console.log(allOrders);
+  }
 }
 
-//WRITE A CASE FOR IF THE RETURNED OBJECT IS NULL INCASE WE TYPE IN THE WRONG NAME
+// Initiate the function chain
+
+//NOTE: getMeal() contains promises so has to be followed by .then
+getMeal()
+  //
+  .then((meal) => setOrder(mealForThisRound));
